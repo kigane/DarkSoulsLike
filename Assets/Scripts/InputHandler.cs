28578061b88d27f3a6a5245ac4323e20117ef7ea
kigane <huwkigane@outlook.com>
2022-12-18 01:10:42 +0800

    人物移动逻辑+动画

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace DarkSoulsLike
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        // 移动幅度，用于决定人物的行动模式是走还是跑。
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        PlayerControls inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        private void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.Player.Move.performed += inputAction => movementInput = inputAction.ReadValue<Vector2>();
+                inputActions.Player.Camera.performed += inputAction => cameraInput = inputAction.ReadValue<Vector2>();
+            }
+
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        public void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+namespace DarkSoulsLike
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+        Transform cameraTransform;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
+
+        [HideInInspector]
+        public Transform myTransform;
+        [HideInInspector]
+        public AnimatorHandler animatorHandler;
+
+        // 这里new关键字表示显式隐藏基类的同名成员。
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField]
+        float movementSpeed = 5f;
+        [SerializeField]
+        float rotationSpeed = 10f;
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            animatorHandler = GetComponentInChildren<AnimatorHandler>();
+            cameraTransform = Camera.main.transform;
+            myTransform = transform;
+            animatorHandler.Initialize();
+        }
+
+        private void Update()
+        {
+            float delta = Time.deltaTime;
+
+            inputHandler.TickInput(delta);
+
+            // 根据相机朝向和输入移动人物
+            moveDirection = cameraTransform.forward * inputHandler.vertical;
+            moveDirection += cameraTransform.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+
+            float speed = movementSpeed;
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+
+            // Log.Debug("Projected Velocity: " + projectedVelocity);
+            // Log.Debug("NormalVector: " + normalVector);
+
+            rigidbody.velocity = projectedVelocity;
+
+            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
+
+            if (animatorHandler.canRotate)
+            {
+                HandleRotation(delta);
+            }
+        }
+
+        #region Movement
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        /// <summary>
+        /// 人物转向
+        /// </summary>
+        /// <param name="delta"></param>
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDir = cameraTransform.forward * inputHandler.vertical;
+            targetDir += cameraTransform.right * inputHandler.horizontal;
+
+            // normalized会返回标准化后的向量，但原值不会改变。而Normalize()会将原值标准化。
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+                targetDir = myTransform.forward;
+
+            float rs = rotationSpeed;
+
+            Quaternion tr = Quaternion.LookRotation(targetDir);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+
+            // hello
+
+        }
+        #endregion
+
+    }
+}
